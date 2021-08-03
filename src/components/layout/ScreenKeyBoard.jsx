4fe43745636945b0b895e25c9d532ff28246cd8a
Alex <Aleks031994@Mail.ru>
2021-08-03 14:56:41 +0300

    implement styles for keyboard

--- src/components/layout/ScreenKeyBoard.jsx
+++ src/components/layout/ScreenKeyBoard.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 
 const ScreenKeyBoard = () => {
+
+    const keyboardnumber = [1,2,3,4,5,6,7,8,9]
+
     return (
-        <div className='grid grid-cols-3 gap-4'>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
-            <div>5</div>
-            <div>6</div>
-            <div>7</div>
-            <div>8</div>
-            <div>9</div>
-            <div className='col-span-2 text-center'>стереть</div>
-            <div>0</div>
+        <div className='grid grid-cols-3 gap-4 px-14 justify-items-center mt-8'>
+            {
+                keyboardnumber.map(number => {
+                    return <div className='w-20 h-14 flex items-center justify-center border-2 border-gray-900'>{number}</div>
+                })
+            }
+            <div className='col-span-2 text-center flex items-center justify-center border-2 border-gray-900 w-48'>стереть</div>
+            <div className='w-20 h-14 flex items-center justify-center border-2 border-gray-900'>0</div>
         </div>
     );
 };
