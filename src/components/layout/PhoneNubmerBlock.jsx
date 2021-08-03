5e69f07f310791071da7fa67361b691ce828dda5
Alex <Aleks031994@Mail.ru>
2021-08-03 14:39:36 +0300

    add first prototype of screen keyboard

--- src/components/layout/PhoneNubmerBlock.jsx
+++ src/components/layout/PhoneNubmerBlock.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import ScreenKeyBoard from "./ScreenKeyBoard";
 
 const PhoneNumberBlock = () => {
     return (
         <div className='w-1/3 h-screen bg-phoneBlock'>
-
+            <h1>Ввведите ваш номер телефона</h1>
+            <span>и мы свяжемся с вами для дальшейшей консультации</span>
+            <ScreenKeyBoard/>
         </div>
     );
 };
