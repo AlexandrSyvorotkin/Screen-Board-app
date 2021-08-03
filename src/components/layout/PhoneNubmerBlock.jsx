319c1fb51e4adc363b4e4d0b848cee00079ee6a1
Alex <Aleks031994@Mail.ru>
2021-08-03 19:59:11 +0300

    normalize layout and add button + some fixes in components

--- src/components/layout/PhoneNubmerBlock.jsx
+++ src/components/layout/PhoneNubmerBlock.jsx
@@ -5,9 +5,14 @@ import Button from "../UI/Button";
 
 const PhoneNumberBlock = () => {
     return (
-        <div className='w-1/3 h-screen bg-phoneBlock'>
-            <h1>Ввведите ваш номер мобильного телефона</h1>
-            <span>и мы свяжемся с вами для дальшейшей консультации</span>
+        <div className='w-1/3 h-screen bg-phoneBlock px-14'>
+            <h1 className='text-xl'>Ввведите ваш номер мобильного телефона</h1>
+            <span>и с Вами свяжется наш менеджер для дальшейшей консультации</span>
+            <div>
+                <form>
+                    <input type='number'/>
+                </form>
+            </div>
             <ScreenKeyBoard/>
             <AgreementForm/>
             <Button>подтвердить номер</Button>
--- src/components/layout/ScreenKeyBoard.jsx
+++ src/components/layout/ScreenKeyBoard.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const ScreenKeyBoard = () => {
 
-    const keyboardnumber = [1,2,3,4,5,6,7,8,9]
+    const keyboardNumber = [1,2,3,4,5,6,7,8,9]
 
     return (
-        <div className='grid grid-cols-3 gap-4 px-14 justify-items-center mt-8'>
+        <div className='grid grid-cols-3 gap-4 justify-items-center mt-8'>
             {
-                keyboardnumber.map(number => {
-                    return <div className='w-20 h-14 flex items-center justify-center border-2 border-gray-900 hover:bg-gray-900 hover:text-white'>{number}</div>
+                keyboardNumber.map((number) => {
+                    return <div key={number.toString()} className='w-20 h-14 flex items-center justify-center border-2 border-gray-900 hover:bg-gray-900 hover:text-white'>{number}</div>
                 })
             }
             <div className='col-span-2 text-center flex items-center justify-center border-2 border-gray-900 w-48 hover:bg-gray-900 hover:text-white'>стереть</div>
