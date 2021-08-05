cdec309bbc3d7062463cd0354174004fea44a446
Alex <Aleks031994@Mail.ru>
2021-08-05 17:25:44 +0300

    megre branches

--- src/components/layout/PhoneNubmerBlock.jsx
+++ src/components/layout/PhoneNubmerBlock.jsx
@@ -4,7 +4,6 @@ import ScreenKeyBoard from "./ScreenKeyBoard";
 const PhoneNumberBlock = () => {
 
 
-
     return (
         <div className='w-1/3 max-w-screen-md bg-phoneBlock px-14 pt-20'>
             <div className='text-xl text-2xl text-center'>Ввведите ваш номер мобильного телефона</div>
