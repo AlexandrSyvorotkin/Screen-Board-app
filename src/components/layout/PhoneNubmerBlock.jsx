17edd563e2a309280fa8544eb05cbaa889b1f827
Alex <Aleks031994@Mail.ru>
2021-08-05 14:24:00 +0300

    add controled input and connect to keyboard

--- src/components/layout/PhoneNubmerBlock.jsx
+++ src/components/layout/PhoneNubmerBlock.jsx
@@ -1,21 +1,42 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ScreenKeyBoard from "./ScreenKeyBoard";
 import AgreementForm from "./AgreementForm";
 import Button from "../UI/Button";
 
 const PhoneNumberBlock = () => {
 
-    const [input, setInput] = useState('')
+    // const [phoneNumber, setPhoneNumber] = useState([])
+    //
+    // const addNumber = () => {
+    //     setPhoneNumber([...phoneNumber, {
+    //         id: phoneNumber.length,
+    //         value: Math.floor(Math.random() * 10) + 1
+    //     }])
+    // }
+    //
+
+
+    // const onChangeNumber = (e) => {
+    //     setNumberValue(e.target.value)
+    // }
+
+
+
+
 
     return (
         <div className='w-1/3 h-screen bg-phoneBlock px-14'>
             <h1 className='text-xl'>Ввведите ваш номер мобильного телефона</h1>
             <span>и с Вами свяжется наш менеджер для дальшейшей консультации</span>
-            <div>
-                <form>
-                    <input type='number' value={input}/>
-                </form>
-            </div>
+
+            {/*<div className='flex flex-row'>*/}
+            {/*    {*/}
+            {/*        phoneNumber.map((number) => {*/}
+            {/*            return <div  key={number.id}>{number.value}</div>*/}
+            {/*        })*/}
+            {/*    }*/}
+            {/*</div>*/}
+            {/*<ScreenKeyBoard addNumber={addNumber} clearNumbers={clearNumberInput}/>*/}
             <ScreenKeyBoard/>
             <AgreementForm/>
             <Button>подтвердить номер</Button>
--- src/components/layout/ScreenKeyBoard.jsx
+++ src/components/layout/ScreenKeyBoard.jsx
@@ -1,22 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const ScreenKeyBoard = () => {
 
+    const [numberValue, setNumberValue ] = useState('+7')
 
-    const keyboardNumbers = [1,2,3,4,5,6,7,8,9]
+    const addNumber = (number) => (e) => {
+        setNumberValue(numberValue + number)
+    }
 
+    const clearNumberInput = () => {
+        setNumberValue('')
+    }
+
+    const keyboardNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
     return (
+        <>
+            <input type='text' value={numberValue} />
         <div className='grid grid-cols-3 gap-4 justify-items-center mt-8'>
+
             {
                 keyboardNumbers.map((number) => {
-                    return <div key={number.toString()} className='w-20 h-14 flex items-center justify-center border-2 border-gray-900 hover:bg-gray-900 hover:text-white'>{number}</div>
+                    return <button
+                        onClick={addNumber(number)}
+                        key={number.toString()}
+                        className='screenButton'
+                    >
+                        {number}
+                    </button>
                 })
             }
-            <div className='col-span-2 text-center flex items-center justify-center border-2 border-gray-900 w-48 hover:bg-gray-900 hover:text-white'>стереть</div>
-            <div className='w-20 h-14 flex items-center justify-center border-2 border-gray-900 hover:bg-gray-900 hover:text-white'>0</div>
+            <button
+                onClick={clearNumberInput}
+                className='col-span-2 text-center flex items-center justify-center border-2 border-gray-900 w-48 hover:bg-gray-900 hover:text-white'>
+                стереть
+            </button>
+            <button
+                onClick={addNumber(0)}
+                className='screenButton'>
+                0
+            </button>
         </div>
+        </>
     );
 };
+
+
+{/*<button onClick={addNumber(1)} className='screenButton'>1</button>*/}
+{/*<button onClick={addNumber(2)} className='screenButton'>2</button>*/}
+{/*<button onClick={addNumber(3)} className='screenButton'>3</button>*/}
+{/*<button onClick={addNumber(4)} className='screenButton'>4</button>*/}
+{/*<button onClick={addNumber(5)} className='screenButton'>5</button>*/}
+{/*<button onClick={addNumber(6)} className='screenButton'>6</button>*/}
+{/*<button onClick={addNumber(7)} className='screenButton'>7</button>*/}
+{/*<button onClick={addNumber(8)} className='screenButton'>8</button>*/}
+{/*<button onClick={addNumber(9)} className='screenButton'>0</button>*/}
+
+
 
 export default ScreenKeyBoard;
