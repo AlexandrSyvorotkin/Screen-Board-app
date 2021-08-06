1cd7153b7ca3a6a8a4c7b459f4f23218f22bdda3
Alex <Aleks031994@Mail.ru>
2021-08-06 18:20:37 +0300

    implemented close button with tailwind styles

--- src/components/PromoZone.jsx
+++ src/components/PromoZone.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Vod from "../assets/Promo.jpg";
+import BtnClose from '../assets/BtnClose.jpg'
+import Button from "./UI/Button";
 
 const PromoZone = () => {
     return (
-        <>
-            <div>
-                <img src={Vod} alt=""/>
-            </div>
-        </>
+        <div>
+            <img src={Vod} alt=""/>
+            <Button styles={'btnClose'}>
+                <img src={BtnClose} alt=""/>
+            </Button>
+        </div>
     );
 };
 
